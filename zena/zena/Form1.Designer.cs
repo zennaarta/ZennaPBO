6723413b539ca544c424409003ad1ba22312cfe5
alfinahalisaa <alfinahalisa19@gmail.com>
2023-05-20 14:03:51 +0700

    pbo zenna

---
+++ zena/zena/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace zena
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Edit = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Delete = new System.Windows.Forms.DataGridViewImageColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Edit,
+            this.Delete});
+            this.dataGridView1.Location = new System.Drawing.Point(13, 352);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1167, 198);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID Outlet";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(162, 90);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(162, 134);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(170, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nama ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(162, 181);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(228, 142);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Alamat";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(504, 89);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(220, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(414, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "No Telepon";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(528, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 43);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "INSERT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(630, 166);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 43);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "UPDATE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1084, 303);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 43);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "RESET";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(189, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "MASTER OUTLET LAUNDRY";
+            // 
+            // Edit
+            // 
+            this.Edit.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Edit.HeaderText = "";
+            this.Edit.Image = global::zena.Properties.Resources.icons8_edit_50;
+            this.Edit.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Zoom;
+            this.Edit.MinimumWidth = 6;
+            this.Edit.Name = "Edit";
+            this.Edit.ReadOnly = true;
+            this.Edit.Width = 6;
+            // 
+            // Delete
+            // 
+            this.Delete.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Delete.HeaderText = "";
+            this.Delete.Image = global::zena.Properties.Resources.icons8_delete_30;
+            this.Delete.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Zoom;
+            this.Delete.MinimumWidth = 6;
+            this.Delete.Name = "Delete";
+            this.Delete.ReadOnly = true;
+            this.Delete.Width = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1192, 562);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridViewImageColumn Edit;
+        private System.Windows.Forms.DataGridViewImageColumn Delete;
+    }
+}
+
---
+++ zena/zena/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace zena
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class databaseHelper
+        {
+            string connString = "Host=localhost;Username=postgres;Password=1;Database=Laundry";
+            NpgsqlConnection conn;
+
+            public void connect()
+            {
+                if (conn == null)
+                {
+                    conn = new NpgsqlConnection(connString);
+                }
+                conn.Open();
+            }
+
+            public DataTable getData(string sql)
+            {
+                DataTable table = new DataTable();
+                connect();
+                try
+                {
+                    NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(sql, conn);
+                    adapter.Fill(table);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+                return table;
+            }
+
+            public void exc(String sql)
+            {
+                connect();
+                try
+                {
+                    NpgsqlCommand cmd = new NpgsqlCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+                    cmd.Dispose();
+                }
+                catch
+                {
+
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        databaseHelper db = new databaseHelper();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OutletData();
+        }
+
+        private void OutletData()
+        {
+            string sql = "select * from outlet";
+            dataGridView1.DataSource = db.getData(sql);
+
+            dataGridView1.Columns["id_outlet"].HeaderText = "ID Outlet";
+            dataGridView1.Columns["nama"].HeaderText = "Nama";
+            dataGridView1.Columns["alamat"].HeaderText = "Alamat";
+            dataGridView1.Columns["no_telepon"].HeaderText = "No Telepon";
+            dataGridView1.Columns["alamat"].HeaderText = "Alamat";
+            dataGridView1.Columns["edit"].DisplayIndex = 5;
+            dataGridView1.Columns["delete"].DisplayIndex = 5;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sql = $"INSERT INTO outlet(id_outlet,nama,alamat,no_telepon) VALUES ('{textBox1.Text}','{textBox2.Text}','{textBox3.Text}','{textBox4.Text}')";
+            DialogResult dialogResult = MessageBox.Show("APAKAH ANDA YAKIN?", "INSERT DATA OUTLET", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                MessageBox.Show("Berhasil!");
+                db.exc(sql);
+                OutletData();
+                button3.PerformClick();
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                MessageBox.Show("Gagal!");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = true;
+            button1.Enabled = true;
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.Columns[e.ColumnIndex].Name == "Edit")
+            {
+                button1.Enabled = false;
+                textBox1.Enabled = false;
+                textBox1.Text = dataGridView1.Rows[e.RowIndex].Cells["id_outlet"].Value.ToString();
+                textBox2.Text = dataGridView1.Rows[e.RowIndex].Cells["nama"].Value.ToString();
+                textBox3.Text = dataGridView1.Rows[e.RowIndex].Cells["alamat"].Value.ToString();
+                textBox4.Text = dataGridView1.Rows[e.RowIndex].Cells["no_telepon"].Value.ToString();
+            }
+            else if (dataGridView1.Columns[e.ColumnIndex].Name == "Delete")
+            {
+                var id = dataGridView1.Rows[e.RowIndex].Cells["id_outlet"].Value.ToString();
+                string sql = $"delete from outlet where id_outlet = '{id}'";
+
+                DialogResult dialogResult = MessageBox.Show("APAKAH ANDA YAKIN?", "DELETE DATA OUTLET", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    MessageBox.Show("Berhasil!");
+                    db.exc(sql);
+                    OutletData();
+                    button3.PerformClick();
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    MessageBox.Show("Gagal!");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sql = $"update outlet set nama = '{textBox2.Text}', alamat = '{textBox3.Text}', no_telepon = '{textBox4.Text}' where id_outlet = '{textBox1.Text}'";
+
+            DialogResult dialogResult = MessageBox.Show("APAKAH ANDA YAKIN?", "UPDATE DATA USER", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                MessageBox.Show("Berhasil!");
+                db.exc(sql);
+                OutletData();
+                button3.PerformClick();
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                MessageBox.Show("Gagal!");
+            }
+        }
+    }
+}
